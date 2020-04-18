1a007534139f0ecfaf398a1a085a7da9b8a32e70
czf2015 <122385930@qq.com>
2020-04-18 21:56:03 +0800

    ~ 代码优化

--- Node/scaffold/Vue/pages/generate.js
+++ Node/scaffold/Vue/pages/generate.js
@@ -2,33 +2,29 @@ const adapterTpl = require(`${__dirname}/../adapter`)
 const businessTpl = require(`${__dirname}/../business`)
 const pageTpl = require(`${__dirname}/../page`)
 const componentTpl = require(`${__dirname}/../component`)
-const raw = require(`${__dirname}/${process.argv[2]}.json`)
+const rawJSON = require(`${__dirname}/${process.argv[2]}.json`)
 const mkdirp = require('mkdirp')
 const fs = require('fs')
 
 const h = (...components) => components.reduce((page, component) => `${page}-${component.replace(/[A-Z]/, c => c.toLowerCase())}`)
 
-function run(raw) {
-    const {page, business} = raw
+function generate(rawJSON) {
+    const {page, business} = rawJSON
     const relative = `${__dirname}/../../src/pages/${page}`
     mkdirp(`${relative}/business`, err => {
-        fs.writeFile(`${relative}/index.js`, pageTpl(page, business), console.log)
-        fs.writeFile(`${relative}/adapter.js`, adapterTpl(business, 'business'), console.log)
+        fs.writeFile(`${relative}/index.vue`, pageTpl(page, business), console.log)
+        fs.writeFile(`${relative}/adapter.js`, adapterTpl(business), console.log)
 
         business.map(component => {
             if (typeof component === 'string') {
-                mkdirp(`${relative}/business/${component}`, (err) => {
-                    fs.writeFile(`${relative}/business/${component}/index.js`, businessTpl(page, component), console.log)
-                })
+                fs.writeFile(`${relative}/business/${component}.vue`, businessTpl(page, component), console.log)
             } else {
                 mkdirp(`${relative}/business/${component.name}/partials`, (err) => {
-                    fs.writeFile(`${relative}/business/${component.name}/index.js`, businessTpl(page, component.name, component.partials), console.log)
+                    fs.writeFile(`${relative}/business/${component.name}/index.vue`, businessTpl(page, component.name, component.partials), console.log)
                     fs.writeFile(`${relative}/business/${component.name}/adapter.js`, adapterTpl(component.partials), console.log)
                     
                     component.partials.map(partial => {
-                        mkdirp(`${relative}/business/${component.name}/partials/${partial}`, (err) => {
-                            fs.writeFile(`${relative}/business/${component.name}/partials/${partial}/index.js`, componentTpl(h(page, component.name, partial)), console.log)
-                        })
+                        fs.writeFile(`${relative}/business/${component.name}/partials/${partial}.vue`, componentTpl(h(page, component.name, partial)), console.log)
                     })
                 })
             }
@@ -36,4 +32,4 @@ function run(raw) {
     })   
 }
 
-run(raw)
+generate(rawJSON)
