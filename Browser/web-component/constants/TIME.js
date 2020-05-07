e0608f29d167e00808832a2fc557250393b0a348
czf2015 <122385930@qq.com>
2020-05-07 22:28:05 +0800

    + Browser/web-component --web-component框架

--- Browser/web-component/constants/TIME.js
+++ Browser/web-component/constants/TIME.js
@@ -1,19 +1,8 @@
-const SECOND = 1000
-const MINUTE = SECOND * 60
-const HOUR = MINUTE * 60
-const DAY = HOUR * 24
-const WEEK = DAY * 7
-const MONTH = DAY * 30
-const YEAR = DAY * 365
-const INTERVAL = SECOND * 3
-
-module.exports = {
-    SECOND,
-    MINUTE,
-    HOUR,
-    DAY,
-    WEEK,
-    MONTH,
-    YEAR,
-    INTERVAL,
-}
+export const SECOND = 1000
+export const MINUTE = SECOND * 60
+export const HOUR = MINUTE * 60
+export const DAY = HOUR * 24
+export const WEEK = DAY * 7
+export const MONTH = DAY * 30
+export const YEAR = DAY * 365
+export const INTERVAL = SECOND * 3
