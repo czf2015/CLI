abb4b7422ddbe044c36a470d71d70dc06bd95016
czf2015 <122385930@qq.com>
2020-05-09 09:21:07 +0800

    + 增加文档

--- Browser/web-component-framework/app.js
+++ Browser/web-component-framework/app.js
@@ -24,15 +24,19 @@ class AppContainer extends AbstractShadow {
     }
 
     listen() {
-        {
+        {   // 点击click，页面跳转
             const paths = ['/', '/demoPage/test'];
             this.shadow.querySelector('h1').addEventListener('click', () => {             
                 const pathIndex = paths.indexOf(window.location.pathname) + 1
                 window.location.pathname = pathIndex < paths.length ?  paths[pathIndex] : paths[0]
-
-                // 验证单向传递
-                this.shadow.querySelector('app-header').props.operation = window.location.pathname
-
+            })
+        }
+        {   // 点击click，验证单向传递   
+            this.header = this.shadow.querySelector('app-header')        
+            this.shadow.querySelector('.app-container > h1').addEventListener('click', () => {   
+                if (this.header.state.list.length > 3) {
+                    this.header.props.operation = 'sub'
+                }    
             })
         }
     }
