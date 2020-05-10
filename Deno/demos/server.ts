00299012e741cd9c41e178609353f1c0d36239a2
czf2015 <122385930@qq.com>
2020-05-10 22:04:41 +0800

    整理deno代码

--- Deno/demos/server.ts
+++ Deno/demos/server.ts
@@ -5,12 +5,8 @@ for await (const req of s) {
   req.respond({ body: "Hello World\n" });
 }
 
-// await Deno.copyFile("./Dep.js", "./Dep.txt");
 
-// const $ = Deno
-// await $.copyFile("./Dep.js", "./Dep1.txt");
-
-
-// const url = Deno.args[0]
+// const url = args[0]
 // const res = await fetch(url)
-// await Deno.copy(Deno.stdout, res.body)
+// console.log(res.url)
+// // Deno.copy(Deno.stdout, res.url)
