5711a52ebda07a0c211714e55d3ca07a8f4e3eef
czf2015 <122385930@qq.com>
2020-05-10 16:11:42 +0800

    ~ 修改框架结构

--- Browser/web-component-framework/apis/index.js
+++ Browser/web-component-framework/apis/index.js
@@ -1,6 +1,6 @@
 import { API } from '../lib/framework/index.js'
-import User from './models/User'
-import Account from './models/Account'
+import User from './models/User.js'
+import Account from './models/Account.js'
 
 
 export const ACCOUNT = new API(Account)
--- Browser/web-component-framework/pages/demoPage/_demo.js
+++ Browser/web-component-framework/pages/demoPage/_demo.js
@@ -1,9 +1,17 @@
-import { customElementRegister } from '../../lib/framework/index.js'
+import { customElementRegister, Request } from '../../lib/framework/index.js'
 import DataBind from '../../components/DataBind.js'
-
+import { ACCOUNT } from '../../apis/index.js'
 
 class DemoPage extends DataBind {
-
+    listen() {
+        {
+            Request.get(ACCOUNT).then(res => {
+                console.log(res)
+                ACCOUNT.post({id: 0, username: 'c0', password: '00'})
+                    .then(console.log)
+            })
+        }
+    }
 }
 
 customElementRegister({
