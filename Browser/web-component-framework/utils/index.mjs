d7bb7797b48fa44d6aebc1e7041ccd4d20717f00
czf2015 <122385930@qq.com>
2020-05-14 10:27:18 +0800

    ~ 调整框架结构

--- Browser/web-component-framework/utils/index.mjs
+++ Browser/web-component-framework/utils/index.mjs
@@ -1,4 +1,4 @@
-export { keyValues, keyValue, contain, deepCopy, format, adapt } from './Object.js'
-export { html, attrs } from './DOM.js'
-export { execute, integrate } from './Run.js'
-export { isType } from './Type.js'
+export { isType } from './type.js'
+export { execute, integrate } from './run.js'
+export { html, attrs } from './dom.js'
+export { keyValues, keyValue, contain, deepCopy, format, adapt } from './object.js'
