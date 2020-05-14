b044ab187ed36cc6411552f9fb1f4430317fed3e
czf2015 <122385930@qq.com>
2020-05-14 11:09:36 +0800

    ~ 调整框架结构

---
+++ Browser/web-component-framework/plugins/DSV.js
@@ -0,0 +1,43 @@
+export default class DSV {
+    constructor(records, columns/* [{ name, type }] */, separator = ',') {
+        this.columns = columns
+        this.separator = separator
+
+        switch (typeof records) {
+            case 'string':
+                this.dsv = records
+                break
+            default:
+                this.dsv = DSV.encode(records, separator)
+                break
+        }
+    }
+    // table信息编码成dsv
+    static encode(table, separator = ',') {
+        try {
+            return table.map(record => record.join(separator))
+                .join('\n')
+        } catch (e) {
+            throw e
+        }
+    }
+    // dsv格式解码成table
+    decode(separator = /\s*,\s*/) {
+        try {
+            return this.dsv.split(/\r*\n/)
+                .map(record => record.split(separator)
+                    .map((item, idx) => this.columns[idx].type(item)))
+        } catch (e) {
+            throw e
+        }
+    }
+    // 
+    get names() {
+        return this.columns.map(({ name }) => name)
+    }
+    // 
+    get records() {
+        return [this.names, ...this.decode(this.separator)]
+    }
+
+}
---
+++ Browser/web-component-framework/plugins/XML.js
@@ -0,0 +1,49 @@
+export default class XML {
+    constructor(template) {
+        this.template = template
+            .replace(patterns.removeSpace, (_, $1) => $1)
+            .replace(/\s+/g, ' ')
+    }
+    // 
+    toJSON() {
+        const root = {
+            children: []
+        }
+
+        const parents = [root]
+    // console.log(this.template)
+        this.template.split(/\>/).forEach(item => {
+            console.log(item)
+                const patterns = {
+                    startTag: /^\s*[\<]\s*(\S+)(\s*)$/,
+                    shutTag: /^\s*[\<]\s*(\S+)(\s*)[\/]$/,
+                    endTag: /^\s*[\<]\s*[\/](\S+)\s*$/,
+                }
+                Object.entries(patterns).forEach(([tag, re]) => {
+                    const matches = item.match(re)
+                    if (matches) {
+                        switch (tag) {
+                            case 'startTag':
+                            case 'shutTag':
+                                const ele = {
+                                    children: [],/*  */
+                                    attributes: attrs(matches[2], true)
+                                }
+    
+                                parents[0].children.push({ [matches[1]]: ele })
+                                tag === 'startTag' && parents.unshift(ele)
+                                break
+                            case 'endTag':
+                                parents.shift()
+                                break
+                            default:
+                                parents[0].children.push(matches[1])
+                        }
+                    }
+                })
+            })
+        return root
+
+        // 
+    }
+}
